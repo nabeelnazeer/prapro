7685ce5b7941d81961ea129bb449446a377c2572
nabeelnazeer <nabeel777@ug.cusat.ac.in>
2024-04-11 18:22:11 +0530

    download_images

--- task_4/code.py
+++ task_4/code.py
@@ -9,4 +9,26 @@ def download_file(url, local_filename):
         r.raise_for_status()
         with open(local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
-                f.write
+                f.write(chunk)
+
+def download_images(link, output_folder, num_images):
+    if not os.path.exists("links.parquet"):
+        print("downloading parquet file...")
+        download_file(link, "links.parquet")
+
+    table = pq.read_table("links.parquet")
+    links = table.to_pandas()
+
+    os.makedirs(output_folder, exists_ok = True)
+
+    for i, link  in enumerate(links['image_url'][:num_images]):
+        filename = os.path.join(output_folder, f"image_{i}.jpg")
+        if not os.path.exists(filename):
+            print(f"downloading {filename}")
+            response = request.get(link, stream =True)
+            with open(filename, 'wb') as f:
+                for chunk in response.iter_count(chunk_size = 1024):
+                    f.write(chunk)
+        else:
+            print(f"{filename} already exists, skipping. ")
+                             
