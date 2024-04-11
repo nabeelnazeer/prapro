8474754fde4318ab2ec238c5ef85f572b2973d31
nabeelnazeer <nabeel777@ug.cusat.ac.in>
2024-04-11 18:09:56 +0530

    download file

---
+++ task_4/code.py
@@ -0,0 +1,12 @@
+import pyarrow.parquet as pq
+import requests
+import os
+import psutil
+import time
+
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write
